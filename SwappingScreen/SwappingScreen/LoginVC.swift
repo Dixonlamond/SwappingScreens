fa85959cfa110f4acc65085960f8d86bc934b72d
dixonlamond <lamond.dixon@protonmail.com>
2017-02-27 19:13:27 -0700

    setting up the Login and Music VC’s

---
+++ SwappingScreen/SwappingScreen/LoginVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SwappingScreen
+//
+//  Created by Lamond Dixon on 2/23/17.
+//  Copyright © 2017 Lamond Dixon. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+
+
+
+}
+
---
+++ SwappingScreen/SwappingScreen/MusicListVC.swift
@@ -0,0 +1,30 @@
+//
+//  MusicListVC.swift
+//  SwappingScreen
+//
+//  Created by Lamond Dixon on 2/27/17.
+//  Copyright © 2017 Lamond Dixon. All rights reserved.
+//
+
+import UIKit
+
+class MusicListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+      view.backgroundColor = UIColor.blue
+   
+    }
+  
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
