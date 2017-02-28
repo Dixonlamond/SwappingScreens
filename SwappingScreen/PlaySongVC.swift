619f8f9db70466e20d0a1d53060a65efd26bd531
dixonlamond <lamond.dixon@protonmail.com>
2017-02-27 20:18:22 -0700

    songTitleLBl not working. 3rd screen will not show song title?

---
+++ SwappingScreen/PlaySongVC.swift
@@ -0,0 +1,49 @@
+//
+//  playSongVC.swift
+//  SwappingScreen
+//
+//  Created by Lamond Dixon on 2/27/17.
+//  Copyright © 2017 Lamond Dixon. All rights reserved.
+//
+
+import UIKit
+
+class PlaySongVC: UIViewController {
+  
+  private var _selectedSong:String!
+  
+  var selectedSong: String
+  {
+    get
+    {
+       return _selectedSong
+    } set {
+      _selectedSong = newValue
+    }
+  }
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        songTitleLBl.text = _selectedSong
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+  @IBOutlet weak var songTitleLBl: UILabel!
+}
--- SwappingScreen/SwappingScreen/MusicListVC.swift
+++ SwappingScreen/SwappingScreen/MusicListVC.swift
@@ -8,23 +8,36 @@
 
 import UIKit
 
-class MusicListVC: UIViewController {
+class MusicListVC: UIViewController
+{
 
-    override func viewDidLoad() {
+ 
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
       
       view.backgroundColor = UIColor.blue
    
     }
+
+  @IBAction func backButtonPressed(_ sender: AnyObject)
+  {
+    dismiss(animated: true, completion: nil)
+  }
+
+  @IBAction func load3rdScreenpressed(_ sender: Any)
+  {
+    let songTitle = "Don't stop"
+    performSegue(withIdentifier: "PlaySongVC", sender: songTitle)
+  }
   
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+  {
+    if let destination = segue.destination as? PlaySongVC {
+      if let song = sender as? String
+      {
+        destination.selectedSong = song
+      }
     }
-    */
-
+  }
 }
