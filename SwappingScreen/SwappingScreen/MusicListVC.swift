672347001f879251b474110b1d84df21c581f9bb
dixonlamond <lamond.dixon@protonmail.com>
2017-03-27 19:27:24 -0600

    Merge branch 'crazy'

--- SwappingScreen/SwappingScreen/MusicListVC.swift
+++ SwappingScreen/SwappingScreen/MusicListVC.swift
@@ -8,23 +8,38 @@
 
 import UIKit
 
-class MusicListVC: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-      
-      view.backgroundColor = UIColor.blue
-   
-    }
+class MusicListVC: UIViewController
+{
   
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+  
+  override func viewDidLoad()
+  {
+    super.viewDidLoad()
+    view.backgroundColor = UIColor.blue
+  }
+  @IBAction func backButton(_ sender: Any)
+  {
+    dismiss(animated: true, completion: nil)
+  }
+  
+  @IBAction func loadScreen(_ sender: Any)
+  {
+    let songTitle = "Damn Son"
+    performSegue(withIdentifier: "PlaySongVC", sender: songTitle)
+  }
+  
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+  {
+    if let destination = segue.destination as? PlaySongVC
+    {
+      
+      if let song = sender as? String
+      {
+        destination.selectedSong = song
+      }
+      
     }
-    */
-
+    
+  }
+  
 }
